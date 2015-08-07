968990ccfe047b55b09a3155e27cf64748248775
Michael Holt <mholt@reallifechurch.org>
2015-08-07 14:38:01 -0700

    try this

--- modules/people/manifests/mholtrlc.pp
+++ modules/people/manifests/mholtrlc.pp
@@ -34,7 +34,7 @@ class people::mholtrlc {
 
   package { 'Atom':
     provider => compressed_app,
-    source   => 'https://atom.io/download/mac',
+    source   => 'https://s3.amazonaws.com/github-cloud/releases/3228505/d0649c96-3c42-11e5-87d9-befb7f868752.zip?response-content-disposition=attachment%3B%20filename%3Datom-mac.zip&response-content-type=application/octet-stream&AWSAccessKeyId=AKIAISTNZFOVBIJMK3TQ&Expires=1438986860&Signature=KBc1FEyZLP8WBssJZbO%2F1reo%2BN8%3D',
   }
 
 
